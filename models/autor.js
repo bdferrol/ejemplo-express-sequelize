22bb4c189100f2e1c7df7e50f0a1994706985661
Aula <Aula@EQUIP079574.xunta.local>
2020-11-10 12:41:10 +0100

    Ampliación de modelo Autor.

--- models/autor.js
+++ models/autor.js
@@ -5,7 +5,9 @@ class Autor extends Model {
 
     static init(connection) {
         super.init({
-            nombre: DataTypes.STRING
+            nombre: {type: DataTypes.STRING, allowNull: false},
+            email: {type: DataTypes.STRING, allowNull: false, unique: true},
+            password: {type: DataTypes.STRING, allowNull: false},
         }, {
             sequelize: connection,
             modelName: "Autor",
