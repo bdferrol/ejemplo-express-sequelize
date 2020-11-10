466fc1bb9c857527ff8514a579f08593f8e88855
Aula <Aula@EQUIP079574.xunta.local>
2020-11-10 13:20:34 +0100

    Lista y registro de usuario.

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -5,21 +5,31 @@ var Autor = require('../models/autor');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send(Autor.findAll())
+router.get('/', async function(req, res, next) {
+  let usuarios = await Autor.findAll();
+  res.render("lista-usuarios", {usuarios});
 });
 
 router.get('/nuevo', function (req, res) {
-  
+  res.render("registro-usuario");
 })
 
-router.post('/nuevo', function (req, res) {
-  
+router.post('/nuevo', async function (req, res) {
+    // Obtención de los datos del formulario
+    let {nombre, email, password, repassword} = req.body;
+
+    if (password == repassword) {
+      let usuario = new Autor({nombre, email, password});
+      await usuario.save();
+      res.redirect("/usuarios");
+    } else {
+      //TODO: mostrar error
+    }
 })
 
 
 router.get('/:id', function (req, res) {
-  
+  res.render("detalle-usuario");  
 })
 
 router.post('/:id', function (req, res) {
