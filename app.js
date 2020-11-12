db61f9b9780d7e68bf980416f9a1eaa12421cfa2
Jairo Chapela-Martinez <jairochapela@gmail.com>
2020-11-12 13:47:56 +0100

    Mensaje de validación para correo repetido.

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cookieSession({
   name: 'sesion', //nombre de la cookie
-  keys: ["aaaaaaaaaa", "bbbbbbbbb"],  //claves para firmar la cookie
+  keys: ["secret1", "secret2"],  //claves para firmar la cookie
   maxAge: 5 * 60 * 1000 //caducidad en milisegundos
 }))
 
--- models/autor.js
+++ models/autor.js
@@ -6,7 +6,7 @@ class Autor extends Model {
     static init(connection) {
         super.init({
             nombre: {type: DataTypes.STRING, allowNull: false},
-            email: {type: DataTypes.STRING, allowNull: false, unique: true},
+            email: {type: DataTypes.STRING, allowNull: false, unique: {args:true, msg:"Error de correo repetido"}},
             password: {type: DataTypes.STRING, allowNull: false},
         }, {
             sequelize: connection,
